fb5d33f6a06e7b5c90a96af1d8297c12eb913cbe
nassim-nader <nassim.nader@e-polytechnique.ma>
2024-04-15 17:39:47 +0100

    adding navbar login & register Modal

--- index.php
+++ index.php
@@ -8,58 +8,162 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
         *{
             font-family: 'Poppins', sans-serif;
         }
-        .h-fontP{
+        .h-font{
             font-family: 'Merienda', cursive;
+        }
+
+        /* Chrome, Safari, Edge, Opera */
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+          -webkit-appearance: none;
+          margin: 0;
+        }
+
+        /* Firefox */
+        input[type=number] {
+          -moz-appearance: textfield;
         }
 
     </style>
     <title>Metama Hotels</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+  <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
+    <div class="container-fluid">
+      <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Metama</a>
+      <button class="navbar-toggler shaodw-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon "></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-2" href="#">Rooms</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-2" href="#">Facilities</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-2" href="#">Contact Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-2" href="#">About</a>
+          </li>
+        </ul>
+        <div class="d-flex">
+
+          <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
+            Login
+          </button>
+          <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
+            Register
+          </button>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <form>
+          <div class="modal-header">
+          <h5 class="modal-title d-flex align-items-center">
+            <i class="bi bi-person-circle fs-3 me-2"></i>User login
+          </h5>
+          <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="mb-3">
+              <label class="form-label">Email address</label>
+            <input type="email" class="form-control shadow-none">
+            </div>
+            <div class="mb-4">
+              <label class="form-label">Password</label>
+            <input type="password" class="form-control shadow-none">
+            </div>
+            <div class="d-flex align-items-center justify-content-between mb-2">
+              <button type="submit" class="btn btn-dark shadow-none">Login</button>
+              <a href="javascript:void(0)" class="text-secondary text-decoration-none">Forgot passord?</a>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
-</nav>
 
+  <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <form>
+          <div class="modal-header">
+            <h5 class="modal-title d-flex align-items-center">
+              <i class="bi bi-person-fill-add fs-3 me-2"></i>User Registration
+            </h5>
+            <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <span class="badge bg-light text-dark mb-3 text-wrap lh-base">
+              Note: Your details must match your ID (verification ID, passport, driving license,etc...)
+              that will be required during check-in.
+            </span>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Name</label>
+                  <input type="text" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Email</label>
+                  <input type="email" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Phone number</label>
+                  <input type="number" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">picture</label>
+                  <input type="file" class="form-control shadow-none">
+              </div>
+              <div class="col-md-12 p-0 mb-3">
+                  <label class="form-label">Address</label>
+                  <textarea class="form-control shadow-none" rows="1"></textarea>
+              </div>
+              <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">PinCode</label>
+                  <input type="number" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Date of birth</label>
+                  <input type="date" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Password</label>
+                  <input type="password" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">confirm Password</label>
+                  <input type="password" class="form-control shadow-none">
+              </div>
+            </div>
+          </div>
 
+          <div class="text-center my-1">
+            <button type="submit" class="btn btn-dark shadow-none">Register</button>
+          </div>
 
-    
+      
+        </form>
+      </div>
+    </div>
+  </div>
+ 
 </body>
 </html>
