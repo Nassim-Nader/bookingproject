0851ccf2b710e02217927ef38e4e99555fe33a7d
nassim-nader <nassim.nader@e-polytechnique.ma>
2024-04-16 18:12:24 +0100

    adding our facilities code

--- index.php
+++ index.php
@@ -396,6 +396,35 @@
   </div>
 
 
+  <!-- Our Facilities -->
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR FACILITIES</h2>
+  <div class="container">
+    <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/wifi.svg" width="80px">
+        <h5 class="mt-3">wifi</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/AC.svg" width="80px">
+        <h5 class="mt-3">Air Condition</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/television.svg" width="80px">
+        <h5 class="mt-3">television</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/roomheater.svg" width="80px">
+        <h5 class="mt-3">Sauna room</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/massage.svg" width="80px">
+        <h5 class="mt-3">massage&spa</h5>
+      </div>
+      <div class="col-lg-12 text-center mt-5">
+        <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities -></a>
+      </div>
+    </div>
+  </div>
 
   <br><br><br>
   <br><br><br>
