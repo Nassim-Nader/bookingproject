ae778a4d13efa303c0a26d7d804771fd6bfbd2fb
nassim-nader <nassim.nader@e-polytechnique.ma>
2024-04-16 16:30:18 +0100

    check avaibility form

--- index.php
+++ index.php
@@ -1,41 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-      <style>
-        *{
-            font-family: 'Poppins', sans-serif;
-        }
-        .h-font{
-            font-family: 'Merienda', cursive;
-        }
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+  <style>
+    * {
+      font-family: 'Poppins', sans-serif;
+    }
 
-        /* Chrome, Safari, Edge, Opera */
-        input::-webkit-outer-spin-button,
-        input::-webkit-inner-spin-button {
-          -webkit-appearance: none;
-          margin: 0;
-        }
+    .h-font {
+      font-family: 'Merienda', cursive;
+    }
 
-        /* Firefox */
-        input[type=number] {
-          -moz-appearance: textfield;
-        }
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
 
-    </style>
-    <title>Metama Hotels</title>
+    /* Firefox */
+    input[type=number] {
+      -moz-appearance: textfield;
+    }
+
+    .custom-bg {
+      background-color: #8DD3BB;
+    }
+
+    .custom-bg:hover {
+      background-color: #279e8c;
+    }
+
+    .avaibility-form {
+      margin-top: -50px;
+      z-index: 2;
+      position: relative;
+    }
+
+    @media screen and (max-width: 575px) {
+      .avaibility-form {
+        margin-top: 25px;
+        padding: 0 35px;
+      }
+    }
+  </style>
+  <title>Metama Hotels</title>
 </head>
+
 <body class="bg-light">
+  <!--navbar-->
   <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
     <div class="container-fluid">
       <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Metama</a>
@@ -72,51 +94,26 @@
       </div>
     </div>
   </nav>
-
-  <div class="container-fluid px-lg mt-4">
-    <div class="swiper swiper-container">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <img src="images/carousel/1.png" class="w-100 d-block"/>
-        </div>
-        <div class="swiper-slide">
-          <img src="images/carousel/2.png" class="w-100 d-block"/>
-        </div>
-        <div class="swiper-slide">
-          <img src="images/carousel/3.png" class="w-100 d-block"/>
-        </div>
-        <div class="swiper-slide">
-          <img src="images/carousel/4.png" class="w-100 d-block"/>
-        </div>
-        <div class="swiper-slide">
-          <img src="images/carousel/5.png" class="w-100 d-block"/>
-        </div>
-        <div class="swiper-slide">
-          <img src="images/carousel/6.png" class="w-100 d-block"/>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
+  <!--modal used for the register and login button in the navbar-->
+  <!--login-->
   <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <form>
           <div class="modal-header">
-          <h5 class="modal-title d-flex align-items-center">
-            <i class="bi bi-person-circle fs-3 me-2"></i>User login
-          </h5>
-          <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+            <h5 class="modal-title d-flex align-items-center">
+              <i class="bi bi-person-circle fs-3 me-2"></i>User login
+            </h5>
+            <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
             <div class="mb-3">
               <label class="form-label">Email address</label>
-            <input type="email" class="form-control shadow-none">
+              <input type="email" class="form-control shadow-none">
             </div>
             <div class="mb-4">
               <label class="form-label">Password</label>
-            <input type="password" class="form-control shadow-none">
+              <input type="password" class="form-control shadow-none">
             </div>
             <div class="d-flex align-items-center justify-content-between mb-2">
               <button type="submit" class="btn btn-dark shadow-none">Login</button>
@@ -127,7 +124,7 @@
       </div>
     </div>
   </div>
-
+  <!--register-->
   <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
@@ -146,44 +143,44 @@
           <div class="container-fluid">
             <div class="row">
               <div class="col-md-6 ps-0 mb-3">
-                  <label class="form-label">Name</label>
-                  <input type="text" class="form-control shadow-none">
+                <label class="form-label">Name</label>
+                <input type="text" class="form-control shadow-none">
               </div>
               <div class="col-md-6 p-0 mb-3">
-                  <label class="form-label">Email</label>
-                  <input type="email" class="form-control shadow-none">
+                <label class="form-label">Email</label>
+                <input type="email" class="form-control shadow-none">
               </div>
               <div class="col-md-6 ps-0 mb-3">
-                  <label class="form-label">Phone number</label>
-                  <input type="number" class="form-control shadow-none">
+                <label class="form-label">Phone number</label>
+                <input type="number" class="form-control shadow-none">
               </div>
               <div class="col-md-6 p-0 mb-3">
-                  <label class="form-label">picture</label>
-                  <input type="file" class="form-control shadow-none">
+                <label class="form-label">picture</label>
+                <input type="file" class="form-control shadow-none">
               </div>
               <div class="col-md-12 p-0 mb-3">
-                  <label class="form-label">Address</label>
-                  <textarea class="form-control shadow-none" rows="1"></textarea>
+                <label class="form-label">Address</label>
+                <textarea class="form-control shadow-none" rows="1"></textarea>
               </div>
               <div class="col-md-6 ps-0 mb-3">
-                  <label class="form-label">PinCode</label>
-                  <input type="number" class="form-control shadow-none">
+                <label class="form-label">PinCode</label>
+                <input type="number" class="form-control shadow-none">
               </div>
               <div class="col-md-6 p-0 mb-3">
-                  <label class="form-label">Date of birth</label>
-                  <input type="date" class="form-control shadow-none">
+                <label class="form-label">Date of birth</label>
+                <input type="date" class="form-control shadow-none">
               </div>
               <div class="col-md-6 ps-0 mb-3">
-                  <label class="form-label">Password</label>
-                  <input type="password" class="form-control shadow-none">
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control shadow-none">
               </div>
               <div class="col-md-6 p-0 mb-3">
-                  <label class="form-label">confirm Password</label>
-                  <input type="password" class="form-control shadow-none">
+                <label class="form-label">confirm Password</label>
+                <input type="password" class="form-control shadow-none">
               </div>
             </div>
           </div>
-
+          </div>
           <div class="text-center my-1">
             <button type="submit" class="btn btn-dark shadow-none">Register</button>
           </div>
@@ -191,19 +188,92 @@
       </div>
     </div>
   </div>
- 
+
+  <!--carousel-->
+  <div class="container-fluid px-lg mt-4">
+    <div class="swiper swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="images/carousel/1.png" class="w-100 d-block" />
+        </div>
+        <div class="swiper-slide">
+          <img src="images/carousel/2.png" class="w-100 d-block" />
+        </div>
+        <div class="swiper-slide">
+          <img src="images/carousel/3.png" class="w-100 d-block" />
+        </div>
+        <div class="swiper-slide">
+          <img src="images/carousel/4.png" class="w-100 d-block" />
+        </div>
+        <div class="swiper-slide">
+          <img src="images/carousel/5.png" class="w-100 d-block" />
+        </div>
+        <div class="swiper-slide">
+          <img src="images/carousel/6.png" class="w-100 d-block" />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- the check avaibility form -->
+
+  <div class="container avaibility-form">
+    <div class="row">
+      <div class="col-lg-12 bg-white shadow p-4 rounded">
+        <h5 class="mb-4">Check Booking Avaibility</h5>
+        <form>
+          <div class="row align-items-end">
+            <div class="col-lg-3 mb-3">
+              <label class="form-label" style="font-weight:500;">Check-in</label>
+              <input type="date" class="form-control shadow-none">
+            </div>
+            <div class="col-lg-3 mb-3">
+              <label class="form-label" style="font-weight:500;">Check-out</label>
+              <input type="date" class="form-control shadow-none">
+            </div>
+            <div class="col-lg-3 mb-3">
+              <label class="form-label" style="font-weight:500;">Adult</label>
+              <select class="form-select shadow-none">
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <div class="col-lg-2 mb-3">
+              <label class="form-label" style="font-weight:500;">Children</label>
+              <select class="form-select shadow-none">
+                <option value="1">zero</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <div class="col-lg-1 mt-2 mb-lg-3">
+              <button type="sybmit" class="btn text-white shadow-none custom-bg">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <br><br><br>
+  <br><br><br>
+
+
 
   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
   <script>
     var swiper = new Swiper(".swiper-container", {
       spaceBetween: 30,
       effect: "fade",
-      loop:true,
-      autoplay:{
-        delay:3500,
-        disableOnInteraction:false,
+      loop: true,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
       }
     });
   </script>
 </body>
+
 </html>
