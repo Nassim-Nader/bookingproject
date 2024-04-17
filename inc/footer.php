4aa26ed1deec3b4c16c041efa955ce04e964ea8b
nassim-nader <nassim.nader@e-polytechnique.ma>
2024-04-17 20:17:21 +0100

    seperating the header and footer and links for better organisation

---
+++ inc/footer.php
@@ -0,0 +1,40 @@
+<!-- Footer-->
+<div class="container-fluid bg-white mt-5">
+    <div class="row">
+      <div class="col-lg-4">
+        <h3 class="h-font fw-bold fs-3 mb-2">Metama</h3>
+        <p>Eiusmod non in Lorem nisi do ea ad sunt amet quis consectetur minim adipisicing. 
+          Ut eu officia deserunt occaecat voluptate eiusmod sit aliqua proident ad. 
+          Aliquip incididunt reprehenderit aliquip consequat duis.</p>
+      </div>
+      <div class="col-lg-4">
+        <h5 class="mb-3">Links</h5>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+      </div>
+      <div class="col-lg-4">
+        <h5 class="mb-3">Fallow us</h5>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+            <i class="bi bi-twitter-x me-1"></i>
+            Twitter-X
+        </a>
+        <br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+            <i class="bi bi-facebook me-1"></i>
+            Facebook
+        </a>
+        <br>
+        <a href="#" class="d-inline-block text-dark text-decoration-none">
+            <i class="bi bi-instagram me-1"></i>
+            Instagram
+        </a>
+        <br>
+       
+      </div>
+    </div>
+  </div>
+
+  <h6 class="text-center bg-dark text-white p-3 m-0">Designed and developed by Metama</h6>
