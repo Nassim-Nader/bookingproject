f4ea3af4503061cac10e3348acab2518c0e37224
nassim-nader <nassim.nader@e-polytechnique.ma>
2024-04-17 10:01:25 +0100

    adding footer and almost finishing homepage

--- index.php
+++ index.php
@@ -510,6 +510,9 @@
       </div>
       <div class="swiper-pagination"></div>
     </div>
+    <div class="col-lg-12 text-center mt-5">
+      <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">see more reviews -></a>
+    </div>
   </div>
 
   <!-- Contact Us -->
@@ -557,10 +560,47 @@
     </div>
   </div>
 
-  <br><br><br>
-  <br><br><br>
 
+  <!-- Footer-->
+  <div class="container-fluid bg-white mt-5">
+    <div class="row">
+      <div class="col-lg-4">
+        <h3 class="h-font fw-bold fs-3 mb-2">Metama</h3>
+        <p>Eiusmod non in Lorem nisi do ea ad sunt amet quis consectetur minim adipisicing. 
+          Ut eu officia deserunt occaecat voluptate eiusmod sit aliqua proident ad. 
+          Aliquip incididunt reprehenderit aliquip consequat duis.</p>
+      </div>
+      <div class="col-lg-4">
+        <h5 class="mb-3">Links</h5>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+      </div>
+      <div class="col-lg-4">
+        <h5 class="mb-3">Fallow us</h5>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+            <i class="bi bi-twitter-x me-1"></i>
+            Twitter-X
+        </a>
+        <br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+            <i class="bi bi-facebook me-1"></i>
+            Facebook
+        </a>
+        <br>
+        <a href="#" class="d-inline-block text-dark text-decoration-none">
+            <i class="bi bi-instagram me-1"></i>
+            Instagram
+        </a>
+        <br>
+       
+      </div>
+    </div>
+  </div>
 
+  <h6 class="text-center bg-dark text-white p-3 m-0">Designed and developed by Metama</h6>
 
   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
   <script>
