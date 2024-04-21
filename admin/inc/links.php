c4fedbb0c906c8dd5b8f406e95fb20462c61d9c7
nassim-nader <nassim.nader@e-polytechnique.ma>
2024-04-21 16:54:31 +0100

    adding admin sidebar and putting both in new file the header so i won't need to make it every time

--- admin/inc/links.php
+++ admin/inc/links.php
@@ -4,4 +4,4 @@
   <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-  <link rel="stylesheet" href="css/nad.css">
+  <link rel="stylesheet" href="css/comman.css">
