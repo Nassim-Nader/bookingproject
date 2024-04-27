b72ebd5cd7fa58f36ac5f9198929ece065416f2d
nassim-nader <nassim.nader@e-polytechnique.ma>
2024-04-27 13:05:48 +0100

    facility section front-end so it will be dynamic

--- facilities.php
+++ facilities.php
@@ -4,113 +4,59 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php require('inc/links.php')?>
+  <?php require ('inc/links.php') ?>
 
-  
-  
+
+
   <title>Metama Hotels - facilities</title>
 
   <style>
-    .pop:hover{
+    .pop:hover {
       border-top-color: var(--teal) !important;
       transform: scale(1.03);
       transition: all 0.3s;
     }
-
   </style>
 </head>
 
 <body class="bg-light">
 
-  <?php require('inc/header.php');?>
+  <?php require ('inc/header.php'); ?>
 
   <div class="my-5 px-4">
     <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
     <div class="h-line bg-dark"></div>
     <p class="text-center mt-3">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Ipsa itaque aperiam,<br> id ipsum  numquam consectetur. 
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Ipsa itaque aperiam,<br> id ipsum numquam consectetur.
       Ipsa sed voluptate ut dolore.</p>
   </div>
 
   <div class="container">
     <div class="row">
-      <div class="col-lg-4 col-md-6 mb-5 px-4">
-        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-          <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/wifi.svg" width="40px">
-          <h5 class="m-0 ms-3">WIFI</h5>
-          </div>  
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Exercitationem ipsa animi voluptatibus facilis sequi a alias.
-          </p>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 mb-5 px-4">
-        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-          <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/AC.svg" width="40px">
-          <h5 class="m-0 ms-3">AIR CONDITION</h5>
-          </div>  
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Exercitationem ipsa animi voluptatibus facilis sequi a alias.
-          </p>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 mb-5 px-4">
-        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-          <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/massage.svg" width="40px">
-          <h5 class="m-0 ms-3">MASSAGE</h5>
-          </div>  
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Exercitationem ipsa animi voluptatibus facilis sequi a alias.
-          </p>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 mb-5 px-4">
-        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-          <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/roomheater.svg" width="40px">
-          <h5 class="m-0 ms-3">ROOM HEATER</h5>
-          </div>  
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Exercitationem ipsa animi voluptatibus facilis sequi a alias.
-          </p>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 mb-5 px-4">
-        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-          <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/television.svg" width="40px">
-          <h5 class="m-0 ms-3">TELEVISION</h5>
-          </div>  
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Exercitationem ipsa animi voluptatibus facilis sequi a alias.
-          </p>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 mb-5 px-4">
-        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-          <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/sauna.svg" width="40px">
-          <h5 class="m-0 ms-3">sauna room</h5>
-          </div>  
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Exercitationem ipsa animi voluptatibus facilis sequi a alias.
-          </p>
-        </div>
-      </div>
+      
+      <?php 
+        $res = selectAll('facilities');
+        $path = FACILITIES_IMG_PATH;
+
+        while ($row = mysqli_fetch_array($res)) {
+          echo<<<data
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+              <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                  <img src="$path$row[icon]" width="40px">
+                  <h5 class="m-0 ms-3">$row[name]</h5>
+                </div>
+                <p>$row[description]</p>
+              </div>
+            </div>
+          data;
+        }
+      ?>
     </div>
   </div>
 
-  <?php require('inc/footer.php');?>
+  <?php require ('inc/footer.php'); ?>
 
 
 </body>
