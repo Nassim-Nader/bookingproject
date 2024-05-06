df6e51fa4dd30d2114a3d95dec3724347d75e2b6
nassim-nader <nassim.nader@e-polytechnique.ma>
2024-05-06 18:56:39 +0100

    booking Availibilty validations

--- confirm_booking.php
+++ confirm_booking.php
@@ -74,17 +74,17 @@
 
       <div class="col-lg-7 col-md-12 px-4">
         <?php
-          $room_thumb = ROOMS_IMG_PATH . "thumbnail.jpg";
-          $thumb_q = mysqli_query($con, "SELECT * FROM `room_images` 
+        $room_thumb = ROOMS_IMG_PATH . "thumbnail.jpg";
+        $thumb_q = mysqli_query($con, "SELECT * FROM `room_images` 
             WHERE `room_id`='$room_data[id]' 
             AND `thumb` = '1'");
 
-          if (mysqli_num_rows($thumb_q) > 0) {
-            $thumb_res = mysqli_fetch_assoc($thumb_q);
-            $room_thumb = ROOMS_IMG_PATH . $thumb_res['image'];
-          }
+        if (mysqli_num_rows($thumb_q) > 0) {
+          $thumb_res = mysqli_fetch_assoc($thumb_q);
+          $room_thumb = ROOMS_IMG_PATH . $thumb_res['image'];
+        }
 
-          echo<<<data
+        echo <<<data
             <div class="card p-3 shadow-sm rounded">
              <img src="$room_thumb" class="img-fluid rounded mb-3">
              <h5>$room_data[name]</h5>
@@ -97,40 +97,46 @@
       <div class="col-lg-5 col-md-12 px-4">
         <div class="card mb-4 border-0 shadow-sm rounded-3">
           <div class="card-body">
-              <form action="#">
-                <h6 class="mb-3">BOOKING DETAILS</h6>
-                <div class="row">
-                  <div class="col-md-6 mb-3">
-                    <label class="form-label">Name</label>
-                    <input name="name" type="text" value="<?php echo $user_data['name'] ?>" class="form-control shadow-none" required>
-                  </div>
-                  <div class="col-md-6 mb-3">
-                    <label class="form-label">Phone Number</label>
-                    <input name="phonenum" type="number" value="<?php echo $user_data['phonenum'] ?>"  class="form-control shadow-none" required>
-                  </div>
-                  <div class="col-md-12 mb-3">
-                    <label class="form-label">Address</label>
-                    <textarea name="address" class="form-control shadow-none" rows="1" required><?php echo $user_data['address'] ?></textarea>
-                  </div>
-                  <div class="col-md-6 mb-3">
-                    <label class="form-label">Check-in</label>
-                    <input name="checkin" type="date" class="form-control shadow-none" required>
-                  </div>
-                  <div class="col-md-6 mb-4">
-                    <label class="form-label">Check-out</label>
-                    <input name="checkout" type="date" class="form-control shadow-none" required>
-                  </div>
-                  <div class="col-12">
-                    <div class="spinner-border text-info mb-3 d-none" id="info_loader" role="status">
-                      <span class="visually-hidden">Loading...</span>
-                    </div>
-
-                    <h6 class="mb-3 text-danger" id="pay_info" >Provide check-in & check-out date!</h6>
-                    
-                    <button name="pay_now" class="btn w-100 text-white custom-bg shadow-none mb-1" disabled>Pay Now</button>
-                  </div>
+            <form action="#" id="booking_form">
+              <h6 class="mb-3">BOOKING DETAILS</h6>
+              <div class="row">
+                <div class="col-md-6 mb-3">
+                  <label class="form-label">Name</label>
+                  <input name="name" type="text" value="<?php echo $user_data['name'] ?>"
+                    class="form-control shadow-none" required>
                 </div>
-              </form>
+                <div class="col-md-6 mb-3">
+                  <label class="form-label">Phone Number</label>
+                  <input name="phonenum" type="number" value="<?php echo $user_data['phonenum'] ?>"
+                    class="form-control shadow-none" required>
+                </div>
+                <div class="col-md-12 mb-3">
+                  <label class="form-label">Address</label>
+                  <textarea name="address" class="form-control shadow-none" rows="1"
+                    required><?php echo $user_data['address'] ?></textarea>
+                </div>
+                <div class="col-md-6 mb-3">
+                  <label class="form-label">Check-in</label>
+                  <input name="checkin" onchange="check_availability()" type="date" class="form-control shadow-none"
+                    required>
+                </div>
+                <div class="col-md-6 mb-4">
+                  <label class="form-label">Check-out</label>
+                  <input name="checkout" onchange="check_availability()" type="date" class="form-control shadow-none"
+                    required>
+                </div>
+                <div class="col-12">
+                  <div class="spinner-border text-info mb-3 d-none" id="info_loader" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                  </div>
+
+                  <h6 class="mb-3 text-danger" id="pay_info">Provide check-in & check-out date!</h6>
+
+                  <button name="pay_now" class="btn w-100 text-white custom-bg shadow-none mb-1" disabled>Pay
+                    Now</button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -140,6 +146,66 @@
   </div>
 
   <?php require ('inc/footer.php'); ?>
+  <script>
+    let booking_form = document.getElementById('booking_form');
+    let info_loader = document.getElementById('info_loader');
+    let pay_info = document.getElementById('pay_info');
+
+    function check_availability() {
+      let checkin_val = booking_form.elements['checkin'].value;
+      let checkout_val = booking_form.elements['checkout'].value;
+
+      booking_form.elements['pay_now'].setAttribute('disabled', true);
+
+      if (checkin_val != '' && checkout_val != '') 
+      {
+
+        pay_info.classList.add('d-none');
+        pay_info.classList.replace('text-dark','text-danger');
+        info_loader.classList.remove('d-none');
+
+        let data = new FormData();
+
+        data.append('check_availability', '');
+        data.append('check_in', checkin_val);
+        data.append('check_out', checkout_val);
+
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "nas/confirm_booking.php",true);
+
+        xhr.onload = function () {
+          let data =JSON.parse(this.responseText);
+
+          if(data.status == 'check_in_out_equal'){
+            pay_info.innerText = "You connot check-out on the same day!";
+          }
+          else if(data.status == 'check_out_earlier'){
+            pay_info.innerText = "check-out date is earlier than check-in date!";
+          }
+          else if(data.status == 'check_in_earlier'){
+            pay_info.innerText = "check-in date is earlier than today's date!";
+          }
+          else if(data.status == 'unavailable'){
+            pay_info.innerText = "Room not available for this chick-in date!";
+          }
+          else {
+            pay_info.innerHTML = "No. of days: "+data.days+"<br>Total Amount to Pay: "+data.payment+"DH";
+            pay_info.classList.replace('text-danger','text-dark');
+            booking_form.elements['pay_now'].removeAttribute('disabled');
+          }
+
+          pay_info.classList.remove('d-none');
+          info_loader.classList.add('d-none');
+        }
+        xhr.send(data);
+
+      }
+
+    }
+
+
+  </script>
 
 
 </body>
