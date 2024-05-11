911c9bc655423defbdca1869dd7fa6a4d2ba42fe
nassim-nader <nassim.nader@e-polytechnique.ma>
2024-05-11 17:21:26 +0100

    adding refund ttab in admin page and fixing bugs in new booking page

--- admin/nas/refund_bookings.php
+++ admin/nas/refund_bookings.php
@@ -12,9 +12,9 @@ if (isset($_POST['get_bookings'])) {
     $query = "SELECT bo.*, bd.* FROM `booking_order` bo
     INNER JOIN `booking_details` bd ON bo.booking_id = bd.booking_id 
     WHERE (bo.order_id LIKE ? OR bd.phonenum LIKE ? OR bd.user_name LIKE ?)
-    AND (bo.booking_status=? And bo.arrival=?) ORDER BY bo.booking_id ASC";
+    AND (bo.booking_status=? And bo.refund=?) ORDER BY bo.booking_id ASC";
 
-    $res = select($query,["%$frm_data[search]%","%$frm_data[search]%","%$frm_data[search]%","booked",0],'sssss');
+    $res = select($query,["%$frm_data[search]%","%$frm_data[search]%","%$frm_data[search]%","cancelled",0],'sssss');
 
     $i = 1;
     $table_data = "";
@@ -45,24 +45,18 @@ if (isset($_POST['get_bookings'])) {
                 <td>
                     <b>Room:</b> $data[room_name]
                     <br>
-                    <b>Price:</b> $data[price] DH
-                </td>
-                <td>
                     <b>Check-in:</b> $checkin
                     <br>
                     <b>Check-out:</b> $checkout
                     <br>
-                    <b>Paid:</b> $data[trans_amt] DH
-                    <br>
                     <b>Date:</b>$date
                 </td>
                 <td>
-                    <button type='button' onclick='assign_room($data[booking_id])' class='btn text-white btn-sm fw-bold custom-bg shadow-none' data-bs-toggle='modal' data-bs-target='#assign-room'>
-                        <i class='bi bi-check2-square'></i>Assign Room
-                    </button>
-                    <br>
-                    <button type='button' onclick='cancel_booking($data[booking_id])' class='mt-2 btn btn-outline-danger btn-sm fw-bold  shadow-none' >
-                        <i class='bi bi-trash'></i>Cancel Booking
+                    <b>$data[trans_amt] DH</b> 
+                </td>
+                <td>
+                    <button type='button' onclick='refund_booking($data[booking_id])' class='btn btn-success btn-sm fw-bold  shadow-none' >
+                        <i class='bi bi-cash-stack'></i> Refund
                     </button>
                 </td>
             </tr>
@@ -73,31 +67,14 @@ if (isset($_POST['get_bookings'])) {
     echo $table_data;
 }
 
-if (isset($_POST['assign_room'])) {
-    $frm_data = filteration($_POST);
 
-    $query = "UPDATE `booking_order` bo INNER JOIN `booking_details` bd
-    ON bo.booking_id = bd.booking_id
-    SET bo.arrival=?, bd.room_no =?
-    WHERE bo.booking_id = ?";
-
-    $values = [1, $frm_data['room_no'], $frm_data['booking_id']];
-
-    $res = update($query, $values, 'isi'); //it will updare 2 rows so it will return 2
-
-    echo ($res == 2) ? 1 : 0;
-}
-
-
-if (isset($_POST['cancel_booking'])) {
+if (isset($_POST['refund_booking'])) {
 
     $frm_data = filteration($_POST);
 
-    $query = "UPDATE `booking_order` SET `booking_status`=?, `refund` =? WHERE `booking_id` = ?";
-    $values = ['cancelled', 0, $frm_data['booking_id']];
-    $res = update($query, $values, 'sii');
+    $query = "UPDATE `booking_order` SET `refund` =? WHERE `booking_id` = ?";
+    $values = [1, $frm_data['booking_id']];
+    $res = update($query, $values, 'ii');
 
     echo $res;
 }
-
-?>
