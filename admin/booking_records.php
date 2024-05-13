ab5f68a6f2612d2b9d7a85416ebecf7386d75247
nassim-nader <nassim.nader@e-polytechnique.ma>
2024-05-13 18:00:51 +0100

    booking records with live search and pagination

--- admin/booking_records.php
+++ admin/booking_records.php
@@ -12,7 +12,7 @@ adminLogin();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php require('inc/links.php'); ?>
 
-    <title>Admin Panel - Refund Bookings</title>
+    <title>Admin Panel - Bookings Records</title>
 </head>
 
 <body class="bg-light">
@@ -22,14 +22,14 @@ adminLogin();
     <div class="container-fluid" id="main-content">
         <div class="row">
             <div class="col-lg-10 ms-auto p-4 overflow-hidden">
-                <h3 class="mb-4">REFUND BOOKINGS</h3>
+                <h3 class="mb-4">BOOKINGS RECORDS</h3>
 
 
                 <div class="card border-0 shadow mb-4">
                     <div class="card-body">
 
                         <div class="text-end mb-4">
-                            <input type="text" oninput="get_bookings(this.value)" class="form-control shadow-none w-25 ms-auto" placeholder="type to search ..." >
+                            <input type="text" id="search_input" oninput="get_bookings(this.value)" class="form-control shadow-none w-25 ms-auto" placeholder="type to search ...">
                         </div>
 
                         <div class="table-responsive">
@@ -39,7 +39,8 @@ adminLogin();
                                         <th scope="col">#</th>
                                         <th scope="col">User Details</th>
                                         <th scope="col">Room Details</th>
-                                        <th scope="col">Refund Amount</th>
+                                        <th scope="col">Bookings Details</th>
+                                        <th scope="col">Status</th>
                                         <th scope="col">Action</th>
                                     </tr>
                                 </thead>
@@ -47,6 +48,12 @@ adminLogin();
                                 </tbody>
                             </table>
                         </div>
+
+                        <nav >
+                            <ul class="pagination mt-3" id="table-pagination" >
+
+                            </ul>
+                        </nav>
 
                     </div>
                 </div>
@@ -59,7 +66,7 @@ adminLogin();
 
 
     <?php require('inc/scripts.php'); ?>
-    <script src="scripts/refund_bookings.js"></script>
+    <script src="scripts/booking_records.js"></script>
 </body>
 
 </html>
--- admin/inc/header.php
+++ admin/inc/header.php
@@ -30,6 +30,9 @@
                                     <li class="nav-item">
                                         <a class="nav-link text-white" href="refund_bookings.php">Refund Bookings</a>
                                     </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link text-white" href="booking_records.php">Bookings Records</a>
+                                    </li>
                                 </ul>
                             </div>
                         </li>
