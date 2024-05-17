7842ee4bf0bc12315c4ea867b76c0f74d6947da6
nassim-nader <nassim.nader@e-polytechnique.ma>
2024-05-17 05:06:13 +0100

    adding check  booking availability logic didn't take screenshot wasn't necessary

--- nas/confirm_booking.php
+++ nas/confirm_booking.php
@@ -1,13 +1,12 @@
 <?php
-require ('../admin/inc/db_config.php');
-require ('../admin/inc/essentials.php');
-require ('../inc/sendgrid-php/sendgrid-php.php');
+require('../admin/inc/db_config.php');
+require('../admin/inc/essentials.php');
+require('../inc/sendgrid-php/sendgrid-php.php');
 
 date_default_timezone_set("Africa/Casablanca");
 
 
-if (isset($_POST['check_availability'])) 
-{
+if (isset($_POST['check_availability'])) {
     $frm_data = filteration($_POST);
 
     $status = "";
@@ -20,39 +19,51 @@ if (isset($_POST['check_availability']))
     $checkin_date = new DateTime($frm_data['check_in']);
     $checkout_date = new DateTime($frm_data['check_out']);
 
-    if($checkin_date == $checkout_date){
+    if ($checkin_date == $checkout_date) {
         $status = 'check_in_out_equal';
-        $result = json_encode(["status"=>$status]);
-    }
-    else if ($checkout_date < $checkin_date){
+        $result = json_encode(["status" => $status]);
+    } else if ($checkout_date < $checkin_date) {
         $status = 'check_out_earlier';
-        $result = json_encode(["status"=>$status]);
-    }
-    else if ($checkin_date < $today_date){
+        $result = json_encode(["status" => $status]);
+    } else if ($checkin_date < $today_date) {
         $status = 'check_in_earlier';
-        $result = json_encode(["status"=>$status]);
+        $result = json_encode(["status" => $status]);
     }
 
     //check booking availability if status is blank else return the error
 
-    if($status!=''){
+    if ($status != '') {
         echo $result;
-    }
-    else{
+    } else {
         session_start();
-        $_SESSION['room'];
-        
+
+
         //run query to check room is available or not
+        $tb_query = "SELECT COUNT(*) AS `total_bookings` FROM `booking_order`
+         WHERE booking_status=? AND room_id=?
+         ANd check_out > ? AND check_in < ?";
+
+        $values = ['booked', $_SESSION['room']['id'], $frm_data['check_in'], $frm_data['check_out']];
+        $tb_fetch=mysqli_fetch_assoc(select($tb_query,$values,'siss'));
+
+        $rq_result = select("SELECT `quantity` FROM `rooms` WHERE `id`=?",[$_SESSION['room']['id']],'i');
+        $rq_fetch=mysqli_fetch_assoc($rq_result);
         
+        if(($rq_fetch['quantity']-$tb_fetch['total_bookings'])==0){
+            $status = 'unavailable';
+            $result=json_encode(['status'=>$status]);
+            echo $result;
+            exit;
+        }
+
+
         $count_days = date_diff($checkin_date, $checkout_date)->days;
         $payment = $_SESSION['room']['price'] * $count_days;
 
-        $_SESSION['room']['payment']= $payment;
-        $_SESSION['room']['available']= true;
+        $_SESSION['room']['payment'] = $payment;
+        $_SESSION['room']['available'] = true;
 
-        $result = json_encode(["status" => 'available',"days"=>$count_days,"payment"=>$payment]);
+        $result = json_encode(["status" => 'available', "days" => $count_days, "payment" => $payment]);
         echo $result;
     }
 }
-
-?>
